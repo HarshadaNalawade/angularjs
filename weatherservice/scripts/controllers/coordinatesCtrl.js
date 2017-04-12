e7d6f6d8a827075e41053aa6b34239cc3b50cfec
Harshada Nalawade <harshada.nalawade@neosofttech.com>
2017-04-12 11:33:10 +0530

    All weather service files

---
+++ weatherservice/scripts/controllers/coordinatesCtrl.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular
+    .module('weatherApp')
+    .controller('coordinatesCtrl', function($scope, Weather) {
+
+        $scope.checkWeather = function() {
+
+            var coordinates = {
+                lat: $scope.lat,
+                lon: $scope.lon
+            };
+
+            $scope.weather = Weather.getWeather(coordinates);       
+        }
+
+    });
+
